2be206c13d52cef18c6742581d5ed36ca36406ba
rene-fernando <rene1397@yahoo.com>
2024-04-23 21:14:00 -0500

    Logic for CreateActivity.java

--- app/src/main/java/com/example/leftovertracker/CreateActivity.java
+++ app/src/main/java/com/example/leftovertracker/CreateActivity.java
@@ -4,8 +4,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.ComponentActivity;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
 
 public class CreateActivity extends ComponentActivity {
     public void onCreate(Bundle savedInstanceState) {
@@ -20,10 +26,30 @@ public class CreateActivity extends ComponentActivity {
         Button createButton = (Button) findViewById(R.id.createDone);
         createButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                int id = -1;
+
                 EditText itemName = (EditText) findViewById(R.id.itemName);
                 EditText totalCals = (EditText) findViewById(R.id.itemCalories);
                 EditText servings = (EditText) findViewById(R.id.itemServings);
                 EditText daysLeft = (EditText) findViewById(R.id.itemDaysLeft);
+
+                if(validateItemInfo()){
+                    id = createItem();
+
+                    if(id > 0){
+                        finish();
+                    }
+                }
+                else {
+                    itemName.setText("");
+                    totalCals.setText("");
+                    servings.setText("");
+                    daysLeft.setText("");
+                    itemName.setError("All fields must be filled out.");
+                    totalCals.setError("All fields must be filled out.");
+                    servings.setError("All fields must be filled out.");
+                    daysLeft.setError("All fields must be filled out.");
+                }
             }
         });
     }
@@ -39,7 +65,60 @@ public class CreateActivity extends ComponentActivity {
     }
 
     private int createItem() {
-        // placeholder return value for now while we come up with the logic for this portion
-        return 0;
+        EditText nameInput = (EditText) findViewById(R.id.itemName);
+        EditText calInput = (EditText) findViewById(R.id.itemCalories);
+        EditText servingsInput = (EditText) findViewById(R.id.itemServings);
+        EditText daysInput = (EditText) findViewById(R.id.itemDaysLeft);
+        String itemName = nameInput.getText().toString();
+        String itemCalories = calInput.getText().toString();
+        String itemServings = servingsInput.getText().toString();
+        String itemDaysLeft = daysInput.getText().toString();
+
+        File f = new File(getFilesDir().getAbsolutePath() + "/itemsList.txt");
+        OutputStreamWriter w = null;
+        Scanner scan;
+        int id = -1;
+        String str = null;
+        String[] arr;
+
+        // figuring out if file exists
+        if(!f.exists()) {
+            id = 1;
+
+            try {
+                w = new OutputStreamWriter(openFileOutput("itemsList.txt", MODE_PRIVATE));
+                w.write(id + "," + itemName + "," + itemCalories + "," + itemServings + "," + itemDaysLeft);
+                w.close();
+            }
+            catch(IOException e) {
+                Toast.makeText(getBaseContext(), "IOException: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+        }
+        else {
+            try {
+                scan = new Scanner(openFileInput("itemsList.txt"));
+
+                while (scan.hasNextLine()) {
+                    str = scan.nextLine();
+                }
+                if(str != null) {
+                    arr = str.split(",");
+                    if(arr.length == 5) {
+                        id = Integer.parseInt(arr[0]) + 1;
+                    }
+                }
+                scan.close();
+
+                // use append in this case to avoid overwriting existing data
+                w = new OutputStreamWriter(openFileOutput("itemsList.txt", MODE_APPEND));
+                w.append("\n" + id + "," + itemName + "," + itemCalories + "," + itemServings + "," + itemDaysLeft);
+                w.close();
+
+            } catch (IOException e) {
+                Toast.makeText(getBaseContext(), "IOException: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+        return id;
     }
 }

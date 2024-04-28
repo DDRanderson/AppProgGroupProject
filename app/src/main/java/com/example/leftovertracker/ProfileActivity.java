821d998b6037426cf79c000f0b5e64edf760869d
ismoemoe <michelle.powell@my.utsa.edu>
2024-04-28 17:42:32 -0500

    ignore please!
    wasn't letting me pull commits due to changes on my end so I'm commiting what I have to pull the new changes.

--- app/src/main/java/com/example/leftovertracker/ProfileActivity.java
+++ app/src/main/java/com/example/leftovertracker/ProfileActivity.java
@@ -5,7 +5,6 @@ import android.content.res.AssetManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -16,6 +15,7 @@ import java.util.Scanner;
 
 public class ProfileActivity extends ComponentActivity {
 
+
     private Account profileInfo;
     private AssetManager assets;
     @Override
@@ -25,6 +25,7 @@ public class ProfileActivity extends ComponentActivity {
     * */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.homescreen);
         assets = getAssets();
         //setupProfile();

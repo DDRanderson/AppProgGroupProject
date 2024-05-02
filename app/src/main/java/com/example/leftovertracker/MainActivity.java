bb7ec3658a9dbb1317b8c0b9a28d043fcff4d6f5
kcraguini <katherineraguini@yahoo.com>
2024-05-02 10:41:23 -0500

    Add Registration page. Works Fixed the File direction

--- app/src/main/java/com/example/leftovertracker/MainActivity.java
+++ app/src/main/java/com/example/leftovertracker/MainActivity.java
@@ -10,12 +10,13 @@ import android.widget.Toast;
 
 import androidx.activity.ComponentActivity;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
 public class MainActivity extends ComponentActivity {
     //private Button button;
-    private AssetManager assets;
+    //private AssetManager assets;
 
     /*
      * opens program, loads "login.xml"
@@ -24,7 +25,7 @@ public class MainActivity extends ComponentActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        assets = getAssets();
+        //assets = getAssets();
         //Toast.makeText(this, "Hello Worlds", Toast.LENGTH_SHORT).show();
         setupButtons();
     }
@@ -41,19 +42,24 @@ public class MainActivity extends ComponentActivity {
     *
     * */
     private void setupButtons() {
-        Button button1 = (Button) findViewById(R.id.login);
-        Button button2 = (Button) findViewById(R.id.register);
+        Button loginButton = (Button) findViewById(R.id.login);
+        Button registerButton = (Button) findViewById(R.id.register);
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 EditText userText = (EditText) findViewById (R.id.inputName);
                 EditText passText = (EditText) findViewById(R.id.inputPassword);
-                //int id = authenticate(userText.getText().toString(),passText.getText().toString());
-                Account account = authenticate(userText.getText().toString(),passText.getText().toString());
-                if(account != null){
+                int id = authenticate(userText.getText().toString(),passText.getText().toString());
+                //Account account = authenticate(userText.getText().toString(),passText.getText().toString());
+               /* if(account != null){
                     Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
                     //intent.putExtra("id",id);
                     intent.putExtra("account", account);
+                    startActivity(intent);
+                }*/
+                if(id > 0){
+                    Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                    intent.putExtra("id",id);
                     startActivity(intent);
                 }
                 else{
@@ -64,7 +70,7 @@ public class MainActivity extends ComponentActivity {
                 }
             }
         });
-        button2.setOnClickListener(new View.OnClickListener() {
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
@@ -74,38 +80,69 @@ public class MainActivity extends ComponentActivity {
             }
         });
     }
-    /*private int authenticate(String username, String password){
+    private int authenticate(String username, String password){
         Scanner scan;
         String str = "";
         String [] arr = null;
-        boolean authenticated = false;
+        //boolean authenticated = false;
         int id = -1;
+        File f = new File(getFilesDir().getAbsolutePath() + "/login.txt");
 
         try {
-            scan = new Scanner(assets.open("login.txt"));
-            while(scan.hasNext()){
+            if(f.exists()){
+//            scan = new Scanner(assets.open("login.txt"));
+                scan = new Scanner(openFileInput("login.txt"));
+                while (scan.hasNext()) {
 
-                //EditText t = (EditText) findViewById(R.id.inputName);
+                    //EditText t = (EditText) findViewById(R.id.inputName);
 
-                str = scan.nextLine();
-                //t.setText(str);
-                arr = str.split(",");
-                if(username.equalsIgnoreCase(arr[1]) && password.equals(arr[2])){
-                    id = Integer.parseInt(arr[0]);
-                    authenticated = true;
-                    break;
+                    str = scan.nextLine();
+                    //t.setText(str);
+                    arr = str.split(",");
+                    if (username.equalsIgnoreCase(arr[1]) && password.equals(arr[2])) {
+                        id = Integer.parseInt(arr[0]);
+                        //authenticated = true;
+                        break;
+                    }
                 }
+                scan.close();
             }
-            scan.close();
         }
         catch(IOException e){
-            System.out.println("Error: " + e.getMessage());
+            Toast.makeText(this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
         }
+        return id;
+         /*   if(id > 0){
+                try {
+                    scan = new Scanner(assets.open("accounts.txt"));
+                    while(scan.hasNext()){
+
+                        //EditText t = (EditText) findViewById(R.id.inputName);
+
+                        str = scan.nextLine();
+                        //t.setText(str);
+                        arr = str.split(",");
+                        if(Integer.parseInt(arr[0]) == id){
+                            account = new Account(id, arr[1], arr[2]);
+                            return account;
+                        }
+                    }
+                    scan.close();
+                }
+                catch(IOException e){
+                    System.out.println("Error: " + e.getMessage());
+                }
+            }
+
+            return null;*/
+    }
+/*
 
     /*
     * method that checks if username and password are correct
     *   looks in "login.txt" found in assets folder
     * */
+    /*
     private Account authenticate(String username, String password){
         Scanner scan;
         String str = "";
@@ -113,23 +150,26 @@ public class MainActivity extends ComponentActivity {
         boolean authenticated = false;
         int id = -1;
         Account account;
+        File f = new File(getFilesDir().getAbsolutePath() + "/login.txt");
 
         try {
-            scan = new Scanner(assets.open("login.txt"));
-            while(scan.hasNext()){
+            if(f.exists()) {
+                scan = new Scanner(openFileInput("login.txt"));
+                while (scan.hasNext()) {
 
-                //EditText t = (EditText) findViewById(R.id.inputName);
+                    //EditText t = (EditText) findViewById(R.id.inputName);
 
-                str = scan.nextLine();
-                //t.setText(str);
-                arr = str.split(",");
-                if(username.equalsIgnoreCase(arr[1]) && password.equals(arr[2])){
-                    id = Integer.parseInt(arr[0]);
-                    authenticated = true;
-                    break;
+                    str = scan.nextLine();
+                    //t.setText(str);
+                    arr = str.split(",");
+                    if (username.equalsIgnoreCase(arr[1]) && password.equals(arr[2])) {
+                        id = Integer.parseInt(arr[0]);
+                        authenticated = true;
+                        break;
+                    }
                 }
+                scan.close();
             }
-            scan.close();
         }
         catch(IOException e){
             System.out.println("Error: " + e.getMessage());
@@ -159,5 +199,6 @@ public class MainActivity extends ComponentActivity {
         return null;
     }
 
+ */
 
 }
